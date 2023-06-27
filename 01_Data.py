e62db6c54a8888f212f547d6acf61934ace563ff
baamsoo <135572929+baamsoo@users.noreply.github.com>
2023-06-27 15:10:19 +0900

    Delete 01_Data.py

--- 01_Data.py
+++
@@ -1,9 +0,0 @@
-# Data.py (데이터 불러온 곳)
-import streamlit as st
-import common
-
-common.page_config()
-st.title("Data")
-st.dataframe(common.get_sales(),
-             use_container_width=True,
-             hide_index=True)
