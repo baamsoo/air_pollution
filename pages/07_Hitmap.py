d21e21e9bda1be0a54353d84a4a7ab2b2a012eb6
baamsoo <135572929+baamsoo@users.noreply.github.com>
2023-06-27 15:40:22 +0900

    Update 07_Hitmap.py

--- pages/07_Hitmap.py
+++ pages/07_Hitmap.py
@@ -7,19 +7,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 common.page_config()
-​
+
 st.title("Time-Pollution Material Correlation Coeff. and Heatmap")
-​
+
 df = common.get_sales()
 df['Measurement date'] = pd.to_datetime(df['Measurement date'])  # 문자열을 날짜/시간 형식으로 변환
 df['date'] = df.loc[:, "Measurement date"].dt.date  # 날짜 컬럼 생성
 df = df.drop(['Measurement date'], axis=1)
-​
+
 df = df.groupby(['date'], as_index=False).agg({'SO2':'mean', 'NO2':'mean', 'O3':'mean', 'CO':'mean', 'PM10':'mean', 'PM2.5':'mean'})
 df = df.drop(['date'], axis=1)
 # Calculate Pearson's correlation coefficient
 df_air = df.corr()
-​
+
 # 상관계수 히트맵
 plt.figure(figsize=(12, 10))
 ax = sns.heatmap(df_air, annot=True, cmap='coolwarm', annot_kws={"size": 24})
