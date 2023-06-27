b90d547c22874c39fb9d99d57d007681b094c3b0
baamsoo <135572929+baamsoo@users.noreply.github.com>
2023-06-27 17:08:11 +0900

    Update 04_Barplot.py

--- pages/04_Barplot.py
+++ pages/04_Barplot.py
@@ -9,20 +9,7 @@ import common
 common.page_config()
 
 st.title("SO2 barplot")
-st.write("  ")
-st.write("  ")
-st.write("  ")
-st.write("  ")
-st.write("  ")
-st.write("  ")
-st.write("  ")
-st.write("  ")
-st.write("  ")
-st.write("  ")
-st.write("  ")
-st.write("  ")
-st.write("  ")
-st.write("  ")
+st.divider()
 df = common.get_sales()
 
 # SO2 비율이 높은 정보 10개 출력
