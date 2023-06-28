949b0a0695ed4e543f8ddf2c7cc646687f2694cd
baamsoo <135572929+baamsoo@users.noreply.github.com>
2023-06-28 10:07:02 +0900

    Update 08_Regplot.py

--- pages/08_Regplot.py
+++ pages/08_Regplot.py
@@ -35,7 +35,7 @@ with tab1:
 
 
 with tab2:
-  fig, ax = plt.subplots(3, 1, figsize=(5,10))
+  fig, ax = plt.subplots(3, 1, figsize=(5,6))
   
   variables = [('O3', 'SO2'), ('O3', 'NO2'), ('O3', 'CO')]
   for i, (x_var, y_var) in enumerate(variables):
